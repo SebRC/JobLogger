e235592bd1cffad4da103a75dea0978fa91b2af3
Sebastian Refsbæk Christiansen <seb-chris@live.dk>
2023-07-24 15:43:26 +0200

    Add colors to icons in job table

--- src/data/job/jobStatusIcons.tsx
+++ src/data/job/jobStatusIcons.tsx
@@ -4,10 +4,10 @@ import { ReactNode } from "react";
 
 const iconsMap = new Map<JobStatus, ReactNode>();
 
-iconsMap.set(JobStatus.Applied, <Send />);
-iconsMap.set(JobStatus.InterviewScheduled, <CalendarMonth />);
+iconsMap.set(JobStatus.Applied, <Send color="warning" />);
+iconsMap.set(JobStatus.InterviewScheduled, <CalendarMonth color="info" />);
 iconsMap.set(JobStatus.NotApplied, <Search />);
-iconsMap.set(JobStatus.Rejected, <ThumbDown />);
-iconsMap.set(JobStatus.Hired, <Celebration />);
+iconsMap.set(JobStatus.Rejected, <ThumbDown color="error" />);
+iconsMap.set(JobStatus.Hired, <Celebration color="primary" />);
 
 export default iconsMap;
